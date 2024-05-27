106deda29a85277f4a637aa76a7fd3b0fa48ae43
HumanEntity <human_entity@icloud.com>
2024-05-27 20:59:36 +0200

    Basic demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+use dialoguer::Input;
+use wordle::word::Word;
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let test_word = Word::try_from("fuzzy")?;
+    loop {
+        let word: String = Input::new().with_prompt("guess").interact_text().unwrap();
+        let word = Word::try_from(word.as_ref())?;
+        let matches = test_word.match_word(&word);
+        println!("{}", word.format(matches));
+        if word == test_word {
+            break;
+        }
+    }
+    Ok(())
 }
